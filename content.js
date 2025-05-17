a60a12086526a2d6e561848e4835ac752c8bce2b
Yanislav Galyov <y.galyov@gmail.com>
2025-05-17 14:39:21 +0300

    feat: improve logic

--- content.js
+++ content.js
@@ -1,13 +1,28 @@
+let observer = null;
+
 function removeElements() {
   document
     .querySelectorAll(".ytp-ce-element-show")
     .forEach((el) => el.remove());
 }
 
-removeElements();
+function setup() {
+  if (observer) {
+    observer.disconnect();
+    observer = null;
+  }
 
-const observer = new MutationObserver(() => removeElements());
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
+  if (location.pathname === "/watch") {
+    removeElements();
+    observer = new MutationObserver(() => removeElements());
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  }
+}
+
+window.addEventListener("yt-navigate-finish", () => {
+  console.log("YouTube URL changed (yt-navigate-finish):", location.href);
+  setup();
 });
